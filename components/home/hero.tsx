431bfecf83bd4691d33b3d9e658cb8892ac851d1
Nguyễn <duynd@rikkeisoft.com>
2022-05-19 11:30:38 +0700

    update hero

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -8,7 +8,7 @@ export function HeroSection() {
                 <Stack spacing={4} direction={{xs: 'column-reverse' , md: 'row'}} alignItems={{xs: 'center', md: 'flex-start'}} textAlign={{xs: 'center', md: 'start'}}>
                     <Box>
                         <Typography component="h1" variant='h3' fontWeight='bold' mb={{xs: 3.5, md: 5}}>
-                            Hi, I am Duy <br /> Web design
+                            Hi, I am Anya <br /> Web design
                         </Typography>
                         <Typography mb={{xs: 3.5, md: 5}}> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad commodi iusto harum sequi voluptatem? Placeat, vero numquam itaque incidunt similique quasi ex voluptatem et laudantium provident temporibus quam facilis unde!</Typography>
                         <Button size='large'>Dowload CV</Button>
