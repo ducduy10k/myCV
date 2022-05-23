f36965eef74f7a43668e12e4141b3d0ebc7a0453
Nguyễn <duynd@rikkeisoft.com>
2022-05-23 17:25:40 +0700

    --

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,28 +5,38 @@ import { DialogDeleteProject } from '@/components/project/dialog-delete-project'
 import DialogViewProject from '@/components/project/dialog-view-project';
 import { Project } from '@/models';
 import { Add } from '@mui/icons-material';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import { Alert, Box, Button, Container, Snackbar, Stack, Typography } from '@mui/material';
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
-export interface IProjectPageProps {
+export interface IProjectPageProps {}
+
+export interface MSGAlert {
+  msg:string;
+    type: 'success' | 'info' | 'warning' | 'error';
+    open: boolean
 }
 
+
 export default function ProjectPage(props: IProjectPageProps) {
-  const [projectList, setProjectList] = useState<Project[]>([
-  ])
+  const [projectList, setProjectList] = useState<Project[]>([]);
 
-  const [openDialogView, setOpenDialogView] = React.useState(false);
-  const [openDialogEdit, setOpenDialogEdit] = React.useState(false);
-  const [openDialogDelete, setOpenDialogDelete] = React.useState(false);
+  const [openDialogView, setOpenDialogView] = useState(false);
+  const [openDialogEdit, setOpenDialogEdit] = useState(false);
+  const [openDialogDelete, setOpenDialogDelete] = useState(false);
 
-  const [selectedValue, setSelectedValue] = React.useState<Project | null>(null);
+  const [selectedValue, setSelectedValue] = useState<Project | null>(null);
 
-  useEffect(()=>{
-    projectApi.getAll().then((data:any)=>{
-      console.log(data);
-      setProjectList(data)
+  const [msgAlert, setMsgAlert] = useState<MSGAlert>({
+    msg:'', 
+    type: 'success',
+    open: true
+  })
+
+  useEffect(() => {
+    projectApi.getAll().then((data: any) => {
+      setProjectList(data);
     });
-  },[])
+  }, []);
 
   const handleOpenAddDialog = () => {
     setSelectedValue(null);
@@ -43,80 +53,116 @@ export default function ProjectPage(props: IProjectPageProps) {
     // value._id = (projectList.length + 1) + '';
     setProjectList([...projectList, value]);
     setOpenDialogEdit(false);
+    setMsgAlert({
+      'msg':'Add project success',
+      'type': 'success',
+      open: true
+    })
   };
 
   const handleEditProject = (value: Project) => {
-    setProjectList([...projectList].map((project) =>{
-      return (project._id === value._id) ? value : project
-    }));
+    setProjectList(
+      [...projectList].map((project) => {
+        return project._id === value._id ? value : project;
+      })
+    );
     setOpenDialogEdit(false);
   };
 
   const handleDeleteProject = (value: Project) => {
-    setProjectList([...projectList].filter((project)=>{
-      return project._id !== value._id
-    }));
+    setProjectList(
+      [...projectList].filter((project) => {
+        return project._id !== value._id;
+      })
+    );
     setOpenDialogDelete(false);
   };
 
   const handleOpenDialogViewProject = (value: Project) => {
     setSelectedValue(value);
     setOpenDialogView(true);
-  }
+  };
 
   const handleOpenDialogEditProject = (value: Project) => {
     setSelectedValue(value);
     setOpenDialogEdit(true);
-  }
-
+  };
 
   const handleOpenDialogDeleteProject = (value: Project) => {
-    if(!value) return;
+    if (!value) return;
     setSelectedValue(value);
     setOpenDialogDelete(true);
+  };
+
+  const handleCloseAlert =()=>{
+    setMsgAlert({
+      msg:'', 
+      type:'success',
+      open: false
+    })
   }
 
   return (
-    <Box component='section' pt={2} pb={4}>
-     {
-        openDialogView && selectedValue? (<DialogViewProject
+    <Box component="section" pt={2} pb={4}>
+      {openDialogView && selectedValue ? (
+        <DialogViewProject
           selectedValue={selectedValue}
           open={openDialogView}
           onClose={handleClose}
-        />) : ''
-      }
-      {
-        openDialogDelete && selectedValue? (<DialogDeleteProject
+        />
+      ) : (
+        ''
+      )}
+      {openDialogDelete && selectedValue ? (
+        <DialogDeleteProject
           selectedValue={selectedValue}
           open={openDialogEdit}
           onClose={handleClose}
           onDelete={handleDeleteProject}
-        />) : ''
-      }
+        />
+      ) : (
+        ''
+      )}
+
+      <Snackbar open={msgAlert.open} autoHideDuration={6000} onClose={handleCloseAlert}>
+        <Alert severity={msgAlert.type } sx={{ width: '100%' }}  onClose={handleCloseAlert}>
+          This is a success message!
+        </Alert>
+      </Snackbar>
+
       <Container>
-        <Stack direction='row' mb={2} justifyContent='space-between'>
-          <Typography>
-          </Typography>
-          <Button  variant="contained" onClick={handleOpenAddDialog}  sx={{mb:2}} startIcon={<Add/>}>
+        <Stack direction="row" mb={2} justifyContent="space-between">
+          <Typography></Typography>
+          <Button
+            variant="contained"
+            onClick={handleOpenAddDialog}
+            sx={{ mb: 2 }}
+            startIcon={<Add />}
+          >
             Add project
           </Button>
-          {
-            openDialogEdit ? (<DialogEditProject
+          {openDialogEdit ? (
+            <DialogEditProject
               selectedValue={selectedValue}
               open={openDialogEdit}
               onClose={handleClose}
               onAdd={handleAddProject}
               onEdit={handleEditProject}
-
-            />) : ''
-          }
+            />
+          ) : (
+            ''
+          )}
         </Stack>
-        <ProjectList projects={projectList} onEditItemProject={handleOpenDialogEditProject} onDeleteditItemProject={handleOpenDialogDeleteProject} onViewItemProject={handleOpenDialogViewProject} viewType='edit' />
+        <ProjectList
+          projects={projectList}
+          onEditItemProject={handleOpenDialogEditProject}
+          onDeleteditItemProject={handleOpenDialogDeleteProject}
+          onViewItemProject={handleOpenDialogViewProject}
+          viewType="edit"
+        />
       </Container>
     </Box>
   );
 }
-ProjectPage.Layout = MainLayout
+ProjectPage.Layout = MainLayout;
 ProjectPage.isPrivate = true;
-
-// export const getStaticProps: GetStaticProps<I> =
